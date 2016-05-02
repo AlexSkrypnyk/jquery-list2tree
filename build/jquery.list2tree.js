8ecfdb73bd9624981d55279321f4910164016f4f
Alex Skrypnyk <alex.designworks@gmail.com>
2016-05-02 13:15:41 +1000

    First proper commit of the main functionality.

--- build/jquery.list2tree.js
+++ build/jquery.list2tree.js
@@ -7,15 +7,37 @@
  * License: GPL-2.0
  */
 
-(function ($, window, undefined) {
+;(function ($, window, document, undefined) {
   var pluginName = 'list2tree',
     version = '0.0.1';
 
-  function Plugin(element, args, existingPlugin) {
+  function Plugin(element, options) {
     this.name = pluginName;
     this.version = version;
 
-    this.defaults = {};
+    this.STATE_COLLAPSED = 'collapsed';
+    this.STATE_EXPANDED = 'expanded';
+
+    this.CLASS_LEAF_COLLAPSED = 'js-list2tree-leaf-collapsed';
+    this.CLASS_TRIGGER_COLLAPSED = 'js-list2tree-trigger-collapsed';
+    this.CLASS_TRIGGER_EXPANDED = 'js-list2tree-trigger-expanded';
+    this.CLASS_DEPTH = 'js-list2tree-leaf-depth';
+    this.CLASS_TRIGGER = 'js-list2tree-trigger';
+
+    this.TRIGGER_POSITION_BEFORE = 'before';
+    this.TRIGGER_POSITION_AFTER = 'after';
+
+    this.options = $.extend({}, {
+      leafSelector: '.js-list2tree-leaf',
+      prefix: '-',
+      wrapperDepth: 0,
+      depthClass: 'leaf-depth',
+      triggerExpandedClass: 'trigger-expanded',
+      triggerCollapsedClass: 'trigger-collapsed',
+      triggerPosition: this.TRIGGER_POSITION_BEFORE,
+      state: this.STATE_EXPANDED,
+      removePrefix: true
+    }, options);
 
     this.$element = $(element);
 
@@ -24,23 +46,195 @@
 
   $.extend(Plugin.prototype, {
     init: function () {
-      console.log('Lis2Tree initialised');
+      var self = this;
+      // Process depths of all elements. Depths are used for all structural
+      // interactions.
+      self.processDepths();
+
+      if (self.options.removePrefix) {
+        self.removePrefixes();
+      }
+
+      // Attach triggers to all parents.
+      self.getAllParents().each(function () {
+        var $trigger = self.attachTrigger($(this));
+        if (self.options.state === self.STATE_COLLAPSED) {
+          $trigger.trigger('click');
+        }
+      });
+    },
+    attachTrigger: function ($leaf) {
+      var self = this;
+      var $trigger = self.isExpanded($leaf) ? self.renderTrigger($leaf, self.STATE_EXPANDED) : self.renderTrigger($leaf, self.STATE_COLLAPSED);
+
+      $trigger.on('click', function () {
+        var $this = $(this);
+        var $currentLeaf = $this.parent();
+        if (self.isExpanded($currentLeaf)) {
+          // Collapse all children that are parents.
+          self.getChildren($currentLeaf).each(function () {
+            var $this = $(this);
+            if (self.isParent($this) && self.isExpanded($this)) {
+              self.getTrigger($this).trigger('click');
+            }
+          });
+          self.collapse($currentLeaf);
+          self.renderTrigger($currentLeaf, self.STATE_COLLAPSED);
+        }
+        else {
+          self.expand($currentLeaf);
+          self.renderTrigger($currentLeaf, self.STATE_EXPANDED);
+        }
+      });
+
+      return $trigger;
+    },
+    renderTrigger: function ($leaf, state) {
+      var self = this;
+      var $trigger = self.getTrigger($leaf);
+
+      var classes = [
+        self.CLASS_TRIGGER,
+        state === this.STATE_COLLAPSED ? self.CLASS_TRIGGER_COLLAPSED : self.CLASS_TRIGGER_EXPANDED,
+        state === this.STATE_COLLAPSED ? self.options.triggerCollapsedClass : self.options.triggerExpandedClass
+      ];
+
+      if ($trigger.length === 0) {
+        $trigger = $('<span class="' + classes.join(' ') + '"></span>');
+        if (self.options.triggerPosition === self.TRIGGER_POSITION_BEFORE) {
+          $trigger.prependTo($leaf);
+        }
+        else {
+          $trigger.appendTo($leaf);
+        }
+      }
+      else {
+        $trigger.attr('class', classes.join(' '));
+      }
+
+      return $trigger;
+    },
+    getTrigger: function ($leaf) {
+      return $leaf.find('.' + this.CLASS_TRIGGER);
+    },
+    isCollapsed: function ($leaf) {
+      return $leaf.hasClass(this.CLASS_LEAF_COLLAPSED);
+    },
+    isExpanded: function ($leaf) {
+      return !this.isCollapsed($leaf);
+    },
+    expand: function ($leaf) {
+      this.getChildren($leaf).show();
+      $leaf.removeClass(this.CLASS_LEAF_COLLAPSED);
+    },
+    collapse: function ($leaf) {
+      this.getChildrenAll($leaf).hide();
+      $leaf.addClass(this.CLASS_LEAF_COLLAPSED);
+    },
+    isChecked: function ($el) {
+      return $el.find('input[type="checkbox"]').is(':checked');
+    },
+    processDepths: function () {
+      var self = this;
+      this.getLeaves().each(function () {
+        var $el = $(this);
+        var elementDepth = self.getDepthFromText(self.getText($el), self.options.prefix);
+        $el.addClass(self.CLASS_DEPTH + '-' + elementDepth);
+        $el.addClass(self.options.depthClass + '-' + elementDepth);
+      });
+    },
+    removePrefixes: function () {
+      var self = this;
+      this.getLeaves().each(function () {
+        // console.log($(this));
+        // $(this).css('border', 'solid 1px red');
+        var string = self.getText($(this));
+        while (string.indexOf(self.options.prefix) === 0) {
+          string = string.substr(self.options.prefix.length);
+        }
+        self.setText($(this), string);
+      });
+    },
+    getLeaves: function () {
+      return this.$element.find(this.options.leafSelector);
+    },
+    // Get all parent elements that have children.
+    getAllParents: function () {
+      var self = this;
+      return self.getLeaves().filter(function () {
+        return self.isParent($(this));
+      });
+    },
+    isParent: function ($el) {
+      return this.getChildrenAll($el).length > 0;
+    },
+    // Get only immediate children.
+    getChildren: function ($el) {
+      var self = this;
+      var $children = self.getChildrenAll($el);
+      var depth = self.getDepth($el);
+      $children = $children.filter(function () {
+        return self.getDepth($(this)) === depth + 1;
+      });
+
+      return $children;
+    },
+    // Get all children with grand children etc.
+    getChildrenAll: function ($el) {
+      var self = this;
+      var $children = $();
+      var elementDepth = self.getDepth($el);
+      var $sibling = $el.next();
+
+      while ($sibling.length > 0) {
+        var siblingDepth = self.getDepth($sibling);
+        if (siblingDepth <= elementDepth) {
+          break;
+        }
+        $children = $children.add($sibling);
+        $sibling = $sibling.next();
+      }
+
+      return $children;
+    },
+    getDepth: function ($el) {
+      var depth = 0;
+      var re = new RegExp(this.CLASS_DEPTH + '-([0-9]+)', 'i');
+      var classes = $el.attr('class').split(' ');
+      for (var i in classes) {
+        var match = classes[i].match(re);
+        if (match && match.length > 1) {
+          depth = parseInt(match[1]);
+          break;
+        }
+      }
+      return depth;
+    },
+    getText: function ($el) {
+      return $el.find('label').text();
+    },
+    setText: function ($el, text) {
+      $el.find('label').text(text);
+    },
+    getDepthFromText: function (string, prefix) {
+      var count = 0;
+      while (string.indexOf(prefix) === 0) {
+        count++;
+        string = string.substr(prefix.length);
+      }
+
+      return count;
     }
   });
 
   // Create plugin in jQuery namespace.
-  $.fn[pluginName] = function () {
-    // Get all arguments to pass it to the constructor.
-    var args = arguments;
-    this.each(function () {
-      // Since this plugin maybe applied to the same element multiple times,
-      // we need to pass existing plugin instance to new instance to resolve
-      // any existing bindings and settings.
-      $.data(this, 'plugin_' + pluginName, new Plugin(this, args, $.data(this, 'plugin_' + pluginName)));
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' +
+          pluginName, new Plugin(this, options));
+      }
     });
-
-    return this;
   };
 
 }(jQuery, window));
-
--- test/unit/test.js
+++ test/unit/test.js
@@ -4,39 +4,388 @@
  */
 
 (function () {
-  /**
-   * Asserts field values allowing all events to fire beforehand.
-   */
-  QUnit.assert.fieldValues = function (cb, timeout) {
-    timeout = timeout || 1;
-    stop();
-    setTimeout(function () {
-      cb();
-      start();
-    }, timeout);
+  var classes = {
+    tree: 'js-tree',
+    leaf: 'js-list2tree-leaf',
+    leafDepth: 'js-list2tree-leaf-depth',
+    leafDepthCss: 'leaf-depth',
+    leafCollapsed: 'js-list2tree-leaf-collapsed',
+    trigger: 'js-list2tree-trigger',
+    triggerCollapsed: 'js-list2tree-trigger-collapsed',
+    triggerCollapsedCss: 'trigger-collapsed',
+    triggerExpanded: 'js-list2tree-trigger-expanded',
+    triggerExpandedCss: 'trigger-collapsed',
   };
 
-  /**
-   * Helper to get value from an element.
-   */
-  $.fn.getValue = function () {
-    return $(this).is('input, textarea') ? $(this).val() : $(this).html();
-  };
-
-  /**
-   * Generate random string with n characters of length.
-   */
-  var randomString = function (n) {
-    return new Array(n).join().replace(/(.|$)/g, function () {
-      return ((Math.random() * 36) | 0).toString(36);
+  function getLeafById(id) {
+    var $leaf = null;
+    $('.' + classes.tree).find('.' + classes.leaf).each(function () {
+      if ($(this).find('input').attr('id') === id) {
+        $leaf = $(this);
+        return false;
+      }
     });
+
+    if (!$leaf) {
+      this.ok($leaf, 'Leaf found');
+    }
+    return $leaf;
+  }
+
+  function getTriggerById(id) {
+    var $leaf = getLeafById(id);
+    return $leaf.find('.' + classes.trigger);
+  }
+
+  function clickTrigger(id) {
+    getTriggerById(id).simulate('click');
+  }
+
+  QUnit.assert.fieldDepth = function (id, depth) {
+    var $leaf = getLeafById(id);
+
+    this.ok($leaf.hasClass(classes.leafDepth + '-' + depth));
+    this.ok($leaf.hasClass(classes.leafDepthCss + '-' + depth));
   };
 
-  test('Test positive', function (assert) {
-    var value = randomString(5),
-      $src = $('#text1'),
-      $dst = $('#text2');
+  QUnit.assert.triggerPresent = function (id) {
+    var $trigger = getTriggerById(id);
+    this.ok($trigger.length === 1, 'Trigger attached to the leaf');
+  };
 
-    assert.ok(true, 'Positive test');
+  QUnit.assert.triggerNotPresent = function (id) {
+    var $trigger = getTriggerById(id);
+    this.ok($trigger.length === 0, 'Trigger is not attached to the leaf');
+  };
+
+  QUnit.assert.leafVisible = function (id) {
+    this.ok(getLeafById(id).is(':visible'), 'Leaf is visible');
+  };
+
+  QUnit.assert.leafNotVisible = function (id) {
+    this.notOk(getLeafById(id).is(':visible'), 'Leaf is not visible');
+  };
+
+  QUnit.assert.leafCollapsed = function (id) {
+    this.ok(getLeafById(id).hasClass(classes.leafCollapsed), 'Leaf collapsed class present');
+  };
+
+  QUnit.assert.leafExpanded = function (id) {
+    this.notOk(getLeafById(id).hasClass(classes.leafCollapsed), 'Leaf collapsed class absent');
+  };
+
+  QUnit.assert.triggerCollapsed = function (id) {
+    this.ok(getTriggerById(id).hasClass(classes.triggerCollapsed), 'Trigger collapsed class present');
+    this.notOk(getTriggerById(id).hasClass(classes.triggerExpanded), 'Trigger collapsed class absent');
+  };
+
+  QUnit.assert.triggerExpanded = function (id) {
+    this.ok(getTriggerById(id).hasClass(classes.triggerExpanded), 'Trigger expanded class present');
+    this.notOk(getTriggerById(id).hasClass(classes.triggerCollapsed), 'Trigger collapsed class absent');
+  };
+
+  QUnit.module('Initial processing');
+
+  test('Depths', function (assert) {
+    $('.js-tree').list2tree();
+
+    assert.fieldDepth('leaf-1', 0);
+    assert.fieldDepth('leaf-11', 1);
+    assert.fieldDepth('leaf-12', 1);
+    assert.fieldDepth('leaf-2', 0);
+    assert.fieldDepth('leaf-21', 1);
+    assert.fieldDepth('leaf-22', 1);
+    assert.fieldDepth('leaf-221', 2);
+    assert.fieldDepth('leaf-2221', 3);
+    assert.fieldDepth('leaf-2222', 3);
+    assert.fieldDepth('leaf-2223', 3);
+    assert.fieldDepth('leaf-223', 2);
+    assert.fieldDepth('leaf-23', 1);
+    assert.fieldDepth('leaf-3', 0);
+    assert.fieldDepth('leaf-31', 1);
+    assert.fieldDepth('leaf-32', 1);
+    assert.fieldDepth('leaf-33', 1);
+    assert.fieldDepth('leaf-4', 0);
+    assert.fieldDepth('leaf-5', 0);
+  });
+
+  test('Triggers', function (assert) {
+    $('.js-tree').list2tree();
+
+    assert.triggerPresent('leaf-1');
+    assert.triggerNotPresent('leaf-11');
+    assert.triggerNotPresent('leaf-12');
+    assert.triggerPresent('leaf-2');
+    assert.triggerNotPresent('leaf-21');
+    assert.triggerPresent('leaf-22');
+    assert.triggerNotPresent('leaf-221');
+    assert.triggerNotPresent('leaf-2221');
+    assert.triggerNotPresent('leaf-2222');
+    assert.triggerNotPresent('leaf-2223');
+    assert.triggerNotPresent('leaf-223');
+    assert.triggerNotPresent('leaf-23');
+    assert.triggerPresent('leaf-3');
+    assert.triggerNotPresent('leaf-31');
+    assert.triggerNotPresent('leaf-32');
+    assert.triggerNotPresent('leaf-33');
+    assert.triggerNotPresent('leaf-4');
+    assert.triggerNotPresent('leaf-5');
+  });
+
+  QUnit.module('Expanding/collapsing');
+
+  test('Leaf expand', function (assert) {
+    $('.js-tree').list2tree();
+
+    var id = 'leaf-1';
+    assert.leafExpanded(id);
+    assert.triggerExpanded(id);
+    clickTrigger(id);
+    assert.leafCollapsed(id);
+    assert.triggerCollapsed(id);
+  });
+
+  test('Leaf collapse', function (assert) {
+    $('.js-tree').list2tree({
+      state: 'collapsed'
+    });
+
+    var id = 'leaf-1';
+    assert.leafCollapsed(id);
+    assert.triggerCollapsed(id);
+    clickTrigger(id);
+    assert.leafExpanded(id);
+    assert.triggerExpanded(id);
+  });
+
+  test('Leaf expand/collapse shows/hides children', function (assert) {
+    $('.js-tree').list2tree();
+
+    var parentId = 'leaf-1';
+    var childId1 = 'leaf-11';
+    var childId2 = 'leaf-12';
+    var parentIdSibling = 'leaf-2';
+
+    // Assert initial expanded state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafVisible(childId1);
+    assert.leafVisible(childId2);
+    assert.leafExpanded(parentIdSibling);
+    // Collapse leaf.
+    clickTrigger(parentId);
+    // Assert collapsed state.
+    assert.leafCollapsed(parentId);
+    assert.leafNotVisible(childId1);
+    assert.leafNotVisible(childId2);
+    // Assert that collapsing did not change the state of a sibling parent leaf.
+    assert.leafVisible(parentIdSibling);
+    // Expand leaf.
+    clickTrigger(parentId);
+    // Assert new expanded state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafVisible(childId1);
+    assert.leafVisible(childId2);
+    assert.leafVisible(parentIdSibling);
+  });
+
+  test('Child leaf expand/collapse shows/hides grand children', function (assert) {
+    $('.js-tree').list2tree();
+
+    var parentId = 'leaf-2';
+    var parentIdSibling = 'leaf-3';
+    var childId = 'leaf-22';
+    var childSiblingId1 = 'leaf-21';
+    var childSiblingId2 = 'leaf-23';
+    var grandChildId1 = 'leaf-221';
+    var grandChildId2 = 'leaf-222';
+    var grandChildId3 = 'leaf-223';
+
+    // Assert initial expanded state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafExpanded(childId);
+    assert.triggerExpanded(childId);
+    assert.leafVisible(grandChildId1);
+    assert.leafVisible(grandChildId2);
+    assert.leafVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+    // Collapse leaf.
+    clickTrigger(childId);
+    // Assert collapsed state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafCollapsed(childId);
+    assert.triggerCollapsed(childId);
+    assert.leafNotVisible(grandChildId1);
+    assert.leafNotVisible(grandChildId2);
+    assert.leafNotVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+    // Expand leaf.
+    clickTrigger(childId);
+    // Assert new expanded state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafExpanded(childId);
+    assert.triggerExpanded(childId);
+    assert.leafVisible(grandChildId1);
+    assert.leafVisible(grandChildId2);
+    assert.leafVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+  });
+
+  test('Parent leaf collapses all children and grand children', function (assert) {
+    $('.js-tree').list2tree();
+
+    var parentId = 'leaf-2';
+    var parentIdSibling = 'leaf-3';
+    var childId = 'leaf-22';
+    var childSiblingId1 = 'leaf-21';
+    var childSiblingId2 = 'leaf-23';
+    var grandChildId1 = 'leaf-221';
+    var grandChildId2 = 'leaf-222';
+    var grandChildId3 = 'leaf-223';
+
+    // Assert initial expanded state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafExpanded(childId);
+    assert.triggerExpanded(childId);
+    assert.leafVisible(grandChildId1);
+    assert.leafVisible(grandChildId2);
+    assert.leafVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+    // Collapse leaf.
+    clickTrigger(parentId);
+    // Assert collapsed state.
+    assert.leafCollapsed(parentId);
+    assert.triggerCollapsed(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafNotVisible(childId);
+    assert.leafNotVisible(grandChildId1);
+    assert.leafNotVisible(grandChildId2);
+    assert.leafNotVisible(grandChildId3);
+    assert.leafNotVisible(childSiblingId1);
+    assert.leafNotVisible(childSiblingId2);
+  });
+
+  test('Parent leaf expands previously collapsed all children and grand children', function (assert) {
+    $('.js-tree').list2tree();
+
+    var parentId = 'leaf-2';
+    var parentIdSibling = 'leaf-3';
+    var childId = 'leaf-22';
+    var childSiblingId1 = 'leaf-21';
+    var childSiblingId2 = 'leaf-23';
+    var grandChildId1 = 'leaf-221';
+    var grandChildId2 = 'leaf-222';
+    var grandChildId3 = 'leaf-223';
+
+    // Assert initial expanded state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafExpanded(childId);
+    assert.triggerExpanded(childId);
+    assert.leafVisible(grandChildId1);
+    assert.leafVisible(grandChildId2);
+    assert.leafVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+    // Collapse child leaf.
+    clickTrigger(childId);
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafCollapsed(childId);
+    assert.leafNotVisible(grandChildId1);
+    assert.leafNotVisible(grandChildId2);
+    assert.leafNotVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+    // Collapse parent leaf.
+    clickTrigger(parentId);
+    // Assert collapsed state.
+    assert.leafCollapsed(parentId);
+    assert.triggerCollapsed(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafNotVisible(childId);
+    assert.leafNotVisible(grandChildId1);
+    assert.leafNotVisible(grandChildId2);
+    assert.leafNotVisible(grandChildId3);
+    assert.leafNotVisible(childSiblingId1);
+    assert.leafNotVisible(childSiblingId2);
+    // Expand parent leaf - all previously collapsed fields should stay
+    // collapsed.
+    clickTrigger(parentId);
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafCollapsed(childId);
+    assert.leafVisible(childId);
+    assert.leafNotVisible(grandChildId1);
+    assert.leafNotVisible(grandChildId2);
+    assert.leafNotVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+  });
+
+  test('Parent leaf collapses and expands previously expanded all children and grand children - should be collapsed', function (assert) {
+    $('.js-tree').list2tree();
+
+    var parentId = 'leaf-2';
+    var parentIdSibling = 'leaf-3';
+    var childId = 'leaf-22';
+    var childSiblingId1 = 'leaf-21';
+    var childSiblingId2 = 'leaf-23';
+    var grandChildId1 = 'leaf-221';
+    var grandChildId2 = 'leaf-222';
+    var grandChildId3 = 'leaf-223';
+
+    // Assert initial expanded state.
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafExpanded(childId);
+    assert.triggerExpanded(childId);
+    assert.leafVisible(grandChildId1);
+    assert.leafVisible(grandChildId2);
+    assert.leafVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
+    // Collapse parent leaf.
+    clickTrigger(parentId);
+    // Assert collapsed state.
+    assert.leafCollapsed(parentId);
+    assert.triggerCollapsed(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafNotVisible(childId);
+    assert.leafNotVisible(grandChildId1);
+    assert.leafNotVisible(grandChildId2);
+    assert.leafNotVisible(grandChildId3);
+    assert.leafNotVisible(childSiblingId1);
+    assert.leafNotVisible(childSiblingId2);
+    // Expand parent leaf - all previously non-collapsed fields should stay
+    // collapsed (state is not remember).
+    clickTrigger(parentId);
+    assert.leafExpanded(parentId);
+    assert.triggerExpanded(parentId);
+    assert.leafExpanded(parentIdSibling);
+    assert.leafCollapsed(childId);
+    assert.leafVisible(childId);
+    assert.leafNotVisible(grandChildId1);
+    assert.leafNotVisible(grandChildId2);
+    assert.leafNotVisible(grandChildId3);
+    assert.leafVisible(childSiblingId1);
+    assert.leafVisible(childSiblingId2);
   });
 }());
